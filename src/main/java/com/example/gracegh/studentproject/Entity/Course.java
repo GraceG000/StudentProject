120057a0efc2cc61fbc72710ebc88c746225f821
GraceG000 <graceghansah2000@gmail.com>
2022-12-22 16:53:15 +0000

    Springboot tutorial students application...

--- src/main/java/com/example/gracegh/studentproject/Entity/Course.java
+++ src/main/java/com/example/gracegh/studentproject/Entity/Course.java
@@ -1,4 +1,31 @@
 package com.example.gracegh.studentproject.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Course")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Course {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "course_sequence"
+    )
+    private Long courseId;
+    private String title;
+    private Integer credit;
 }
--- src/main/java/com/example/gracegh/studentproject/Entity/CourseMaterial.java
+++ src/main/java/com/example/gracegh/studentproject/Entity/CourseMaterial.java
@@ -1,4 +1,38 @@
 package com.example.gracegh.studentproject.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CourseMaterial")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CourseMaterial {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_material_sequence",
+            sequenceName = "course_material_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "course_material_sequence"
+    )
+    private Long courseMaterialId;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(
+            name ="course_id",
+            referencedColumnName = "courseId"
+    )//where the foreign key is...
+    private Course course;
+
 }
