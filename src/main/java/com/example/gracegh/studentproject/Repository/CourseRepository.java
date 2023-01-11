685c8dbb91bf26aad4b782abc3f6d1b4b73df101
GraceG000 <graceghansah2000@gmail.com>
2023-01-11 17:02:05 +0000

    Springboot tutorial students application...

--- src/main/java/com/example/gracegh/studentproject/Repository/CourseRepository.java
+++ src/main/java/com/example/gracegh/studentproject/Repository/CourseRepository.java
@@ -1,12 +1,17 @@
 package com.example.gracegh.studentproject.Repository;
 
 import com.example.gracegh.studentproject.Entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-
+ Page<Course> findByTitleContaining(String title, Pageable pageable);
 
 }
